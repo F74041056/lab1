82114f58440447c73dd4729cda7fc0139c527017
TeresaDai <decade910831@gmail.com>
2016-03-13 11:22:56 +0800

    add lab1.cpp into repository lab1

---
+++ lab1.cpp
@@ -0,0 +1,14 @@
+Script started on 2016年03月13日 (週日) 11時20分45秒
+]0;F74041056@2015cpp: ~[01;32mF74041056@2015cpp[00m:[01;34m~[00m$ git clone [K[K[K[K[K[K[K[K[K[Kgit config -l
+user.name=TeresaDai
+user.email=decade910831@gmail.com
+color.ui=true
+core.repositoryformatversion=0
+core.filemode=true
+core.bare=false
+core.logallrefupdates=true
+]0;F74041056@2015cpp: ~[01;32mF74041056@2015cpp[00m:[01;34m~[00m$ git clone https://github.com/F74041056/lab1.git
+fatal: destination path 'lab1' already exists and is not an empty directory.
+]0;F74041056@2015cpp: ~[01;32mF74041056@2015cpp[00m:[01;34m~[00m$ exit
+
+Script done on 2016年03月13日 (週日) 11時21分49秒
